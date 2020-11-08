f07619762b8e7f3a08f9221d1f62a27c2ad7267d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-11-08 16:31:34 +0100

    Implement LCD readout, control logic

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -2,6 +2,8 @@
 
 #include "lcd.h"
 #include "pins.h"
+#include "sensors.h"
+#include "heating.h"
 
 #include "U8glib.h"
 
@@ -29,14 +31,22 @@ void lcd_init()
 
 void lcd_draw()
 {
+    float targetTemp = get_target_temperature();
+    float currentTemp = sensors_get_temperature(0);
+
     u8g.firstPage();
     do
     {
         u8g.setFont(u8g_font_unifont);
+
         u8g.drawFrame(0, 0, 128, 31);
-        u8g.drawStr(15, 13, "Target");
+        u8g.drawStr(10, 20, "Target");
+        u8g.setPrintPos(80, 20);
+        u8g.print(targetTemp, 1);
 
         u8g.drawFrame(0, 33, 128, 31);
-        u8g.drawStr(15, 46, "Current");
+        u8g.drawStr(10, 53, "Current");
+        u8g.setPrintPos(80, 53);
+        u8g.print(currentTemp, 1);
     } while (u8g.nextPage());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,51 @@
 #include <Arduino.h>
 
+#include "sensors.h"
+#include "board_led.h"
+#include "buzzer.h"
+#include "rotary_encoder.h"
 #include "lcd.h"
+#include "heating.h"
 
-void setup() {
+const float DEGREES_PER_ENCODER_STEP = 0.5f;
+
+void setup()
+{
+    sensors_init();
+    rotary_encoder_init();
+
+    board_led_init();
+    buzzer_init();
+
     lcd_init();
 }
 
-void loop() {
-    lcd_draw();
+void loop()
+{
+    // polling
+    sensors_update();
+    rotary_encoder_update();
 
-    delay(50);
+    // handle encoder temperature change
+    static long lastEncoderPos = 0;
+    long encoderPos = rotary_encoder_get_wheel();
+    if (encoderPos != lastEncoderPos)
+    {
+        int diff = (int)(encoderPos - lastEncoderPos);
+        set_target_temperature(get_target_temperature() + DEGREES_PER_ENCODER_STEP * diff);
+        lastEncoderPos = encoderPos;
+    }
+
+    // button test
+    board_led_toggle(rotary_encoder_get_button());
+    buzzer_toggle(rotary_encoder_get_button());
+
+    // LCD update
+    static unsigned long lastLcdDraw = millis();
+    unsigned long now = millis();
+    if (now > lastLcdDraw + 500)
+    {
+        lcd_draw();
+        lastLcdDraw = now;
+    }
 }
